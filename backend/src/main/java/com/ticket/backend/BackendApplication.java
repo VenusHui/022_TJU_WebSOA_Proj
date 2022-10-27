3f6cb1cf211695fa6782b175acf5b4d0add36c46
VenusHui <2913727619@qq.com>
2022-10-27 22:23:01 +0800

    Update layers

--- backend/src/main/java/com/ticket/backend/BackendApplication.java
+++ backend/src/main/java/com/ticket/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package com.ticket.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/ticket/backend/controller/user/UserController.java
+++ backend/src/main/java/com/ticket/backend/controller/user/UserController.java
@@ -1,13 +1,20 @@
 package com.ticket.backend.controller.user;
 
+import com.ticket.backend.mapper.UserMapper;
+import com.ticket.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user/")
 public class UserController {
-    @RequestMapping("getUserInfo/")
-    public String getUserInfo() {
-        return "Hello World";
+
+    @Autowired
+    UserMapper userMapper;
+    @RequestMapping("/user/getAll/")
+    public List<User> getAll() {
+        return userMapper.selectList(null);
     }
 }
