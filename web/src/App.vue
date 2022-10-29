f0939eac2097573e2708e4783776d005899fe529
VenusHui <2913727619@qq.com>
2022-10-29 15:48:22 +0800

    create web frontend

---
+++ web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello World!</div>
+  <router-view></router-view>
+</template>
+
+<script>
+export default{
+  name: "App",
+  setup: () => {
+    
+  }
+}
+</script>
+
+<style>
+</style>
