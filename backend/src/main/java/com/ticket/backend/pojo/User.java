8db9cd94f6530a8d37fc6eaebf1344a8946951e8
VenusHui <2913727619@qq.com>
2022-11-16 16:03:36 +0800

    Update User

--- backend/src/main/java/com/ticket/backend/pojo/User.java
+++ backend/src/main/java/com/ticket/backend/pojo/User.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     private Integer id;
-    private String username;
+    private String user_name;
     private String password;
+    private Integer user_type;
+    private String phone_number;
+    private Date register_date;
+    private String city;
+    private Integer credit;
 }
