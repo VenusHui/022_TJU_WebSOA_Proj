62890db8a4507814753df4f2fe0bdb70f39bd82c
Pillar <77948809+GyxPillar@users.noreply.github.com>
2022-12-23 22:14:11 +0800

    Delete OrderDetailsService.java

--- backendcloud/ticketsystem/src/main/java/com/ticket/ticketsystem/service/OrderDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.ticket.ticketsystem.service;
-
-import java.util.Map;
-
-public interface OrderDetailsService {
-    public Map<String, Object> getOrderDetailsByOrderId(Integer orderId);
-}
