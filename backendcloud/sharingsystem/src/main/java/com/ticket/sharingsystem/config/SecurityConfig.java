544ddad913ac280f550cd55be61c59329b35d7c5
VenusHui <2913727619@qq.com>
2022-12-24 19:52:30 +0800

    Add config of sharing system

---
+++ backendcloud/sharingsystem/src/main/java/com/ticket/sharingsystem/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ticket.sharingsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author: VenusHui
+ * @description: TODO
+ * @date: 2022/12/20 15:18
+ * @version: 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        // add open urls here
+                ).permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated();
+    }
+}
