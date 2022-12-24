4f2fb3b500705cdd6846404b36bb19e50de8e00d
Pillar <wangshangxiake@126.com>
2022-12-24 21:30:17 +0800

    add sharing system pojo

---
+++ backendcloud/sharingsystem/src/main/java/com/ticket/sharingsystem/pojo/Comment.java
@@ -0,0 +1,20 @@
+package com.ticket.sharingsystem.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @TableId(value = "comment_id", type = IdType.AUTO)
+    private Integer commentId;
+    private Integer sharingId;
+    private Integer userId;
+    private String context;
+    private Integer likes;
+}
